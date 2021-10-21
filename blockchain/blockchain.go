5c871188b11d8add3d1cd1fbc8a239ef653b6724
vinylrich <whktjd0109@gmail.com>
2021-10-21 11:10:51 +0900

    4.3 singleton pattern

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,54 +1,20 @@
 package blockchain
 
-import (
-	"crypto/sha256"
-	"fmt"
-	"sync"
-)
-
 type block struct {
-	Data     string
-	Hash     string
-	PrevHash string
+	data     string
+	hash     string
+	prevHash string
 }
 
 type blockchain struct {
-	blocks []*block
+	blocks []block
 }
 
 var b *blockchain
-var once sync.Once //run one time
 
-func (b *block) calculateHash() {
-	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
-	b.Hash = fmt.Sprintf("%x", hash)
-}
-func getLastHash() string {
-	totalBlocks := len(GetBlockchain().blocks)
-	if totalBlocks == 0 {
-		return ""
-	}
-	return GetBlockchain().blocks[totalBlocks-1].Hash
-}
-
-func createBlock(data string) *block {
-	newBlock := block{data, "", getLastHash()}
-	newBlock.calculateHash()
-
-	return &newBlock
-}
-func (b *blockchain) AddBlock(data string) {
-	b.blocks = append(b.blocks, createBlock(data))
-}
-func (b *blockchain) ListOfBlock() []*block {
-	return GetBlockchain().blocks
-}
-func GetBlockchain() *blockchain {
+func GenerateBlockchain() *blockchain {
 	if b == nil {
-		once.Do(func() {
-			b = &blockchain{}
-			b.AddBlock("Genesis Block")
-		})
+		b = &blockchain{}
 	}
 	return b
 }
--- main.go
+++ main.go
@@ -1,49 +1,5 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-)
-
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-func (b *blockchain) getHash(hash string) string {
-	hashed := sha256.Sum256([]byte(hash))
-	return fmt.Sprintf("%x", hashed)
-}
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	newBlock.hash = b.getHash(data + newBlock.prevHash)
-
-	b.blocks = append(b.blocks, newBlock)
-}
-func (b *blockchain) listofBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("PrevHash: %s\n", block.prevHash)
-	}
-}
-
-/*
-b1hash=data+""
-
-b2hash=data+b1hash
-*/
 func main() {
 	chain := blockchain{}
 	chain.addBlock("Genesis Block")
