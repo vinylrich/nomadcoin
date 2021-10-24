c291ecd0f2e41c7d8b635a2ad994bf5152e9b2ad
vinylrich <whktjd0109@gmail.com>
2021-10-24 15:32:34 +0900

    # 6.0 restapi setup

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"nomadcoin/utils"
+)
+
+const port string = ":3000"
+
+type URLDes struct {
+	URL    string
+	Method string
+	Desc   string
+}
+
+func documentation(w http.ResponseWriter, r *http.Request) {
+	data := []URLDes{
+		{
+			URL:    "/",
+			Method: "GET",
+			Desc:   "See Documentation",
+		},
+
+		{
+			URL:    "/block",
+			Method: "POST",
+			Desc:   "Create Block",
+		},
+	}
+	pbyte, err := json.Marshal(data)
+	utils.HandleError(err)
+	fmt.Fprint(w, string(pbyte))
+}
 
 func main() {
-	explorer.Start()
-
+	http.HandleFunc("/", documentation)
+	log.Printf("ListenAndServe http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
