7332e79aef9379f11bf453b61e444be5d5279c31
vinylrich <whktjd0109@gmail.com>
2021-11-12 14:19:28 +0900

    12.4 channel

--- main.go
+++ main.go
@@ -10,29 +10,30 @@ var wait sync.WaitGroup
 
 func countToTen(c chan<- int) {
 	for i := range [10]int{} {
-		fmt.Println(i, c)
+		fmt.Println(">>sending<<", i)
 		c <- i
-		time.Sleep(1 * time.Second)
 	}
 	close(c)
 }
 
 func receive(c <-chan int) { //받기 전용 채널
 	for {
+		time.Sleep(10 * time.Second)
 		a, ok := <-c
 
 		if !ok {
 			fmt.Println("NO VALUE")
 			break
 		}
-		fmt.Println(a)
+		fmt.Println("receive", a)
 
 	}
 
 }
 
+//메세지를 엄청 빠르게 보내고 천천히 처리할 수 있음
 func main() {
-	c := make(chan int)
+	c := make(chan int, 1) //blocking 없이 최대 n개의 값을 보낼 수 있다.
 	go countToTen(c)
 	receive(c)
 }
