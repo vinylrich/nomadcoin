b6734ff17659b481d71dfdc705f987432b100f13
vinylrich <whktjd0109@gmail.com>
2021-11-20 23:35:00 +0900

    12.13 read message to use goroutine

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"nomadcoin/utils"
-	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,24 +14,28 @@ var wsConns []*websocket.Conn
 
 // Port :3000 will upgrade the reqeust from :4000
 func Upgrade(w http.ResponseWriter, r *http.Request) {
+	openPort := r.URL.Query().Get("openPort")
+	ipaddress := utils.Splitter(r.RemoteAddr, ":", 0)
 
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openPort != "" && ipaddress != ""
 	}
-	fmt.Print(r.RemoteAddr)
+
+	fmt.Println(r.RemoteAddr)
 	wsConn, err := upgrader.Upgrade(w, r, nil)
 	utils.HandleError(err)
-	openPort := r.URL.Query().Get("openPort")
-
-	sepedstr := strings.Split(r.RemoteAddr, ":")
-	initPeer(wsConn, sepedstr[0], openPort)
+	initPeer(wsConn, ipaddress, openPort)
+	time.Sleep(20 * time.Second)
+	wsConn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
 }
 
 // Port :4000 is reqeuesting an upgrade form the port :3000
 func AddPeer(address, port, openPort string) {
-	wsConn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	wsConn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleError(err)
-	initPeer(wsConn, address, port)
+	initPeer(wsConn, address, port) //conntect peer
+	time.Sleep(10 * time.Second)
+	wsConn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
 }
 
 /*
--- p2p/peer.go
+++ p2p/peer.go
@@ -17,6 +17,18 @@ func initPeer(conn *websocket.Conn, address, port string) *peer {
 		conn,
 	}
 	key := fmt.Sprintf("%s:%s", address, port)
+	go p.read()
 	Peers[key] = p
 	return p
+}
+
+func (p *peer) read() {
+	//delete peer if error causes
+	for {
+		_, m, err := p.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", m)
+	}
 }
